83d0616ce8e53a58a87546d387717264f2ddd330
kartavya <kartavyas@gmail.com>
2022-04-22 14:04:15 +1000

    updated markdwon file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -54,13 +54,51 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+
+  
+  ## Table of Contents
+  *[License](#license)
+  *[Installation](#installation)
+  *[Usage](#Usage)
+  *[How to Contribute](#how-to-contribute)
+  *[Test](#tests)
+  *[Question??](#questions)
+
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+
+
+  # Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## How to Contribute
+  ${data.contribution}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions??
+  ### Contact me here :
+  [${data.gitHubUsername}](http://github/com/${data.gitHubUsername})
+  ${data.email}`;
+
 }
 
 module.exports = generateMarkdown;
